cb2099694d595ad359689a9b4f6f35593059a2ee
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-12-17 21:55:48 -0800

    .

--- api_test.go
+++ api_test.go
@@ -74,6 +74,7 @@ func TestCreateApiWithNonStruct(t *testing.T) {
 
 	_, _, err = modusdb.Create[*User](context.Background(), db1, &user)
 	require.Error(t, err)
+	require.Equal(t, "expected struct, got ptr", err.Error())
 }
 
 func TestGetApi(t *testing.T) {
