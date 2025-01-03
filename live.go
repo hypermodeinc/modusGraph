8523947b946a7caa393695dfaa867ebcd235c29d
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2025-01-02 17:30:06 -0800

    update

--- live.go
+++ live.go
@@ -1,7 +1,10 @@
 /*
- * This example is part of the ModusDB project, licensed under the Apache License 2.0.
- * You may modify and use this example in accordance with the license.
+ * Copyright 2024 Hypermode Inc.
+ * Licensed under the terms of the Apache License, Version 2.0
  * See the LICENSE file that accompanied this code for further details.
+ *
+ * SPDX-FileCopyrightText: 2024 Hypermode Inc. <hello@hypermode.com>
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package modusdb
