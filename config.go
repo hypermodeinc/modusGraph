998ff1be83722f268e3cd1044888e2816001c0b7
Matthew McNeely <matthew.mcneely@gmail.com>
2025-02-18 16:55:00 -0500

    Add option description

--- config.go
+++ config.go
@@ -16,6 +16,8 @@ func NewDefaultConfig(dir string) Config {
 	return Config{dataDir: dir, limitNormalizeNode: 10000}
 }
 
+// WithLimitNormalizeNode sets the limit for the number of nodes to normalize
+// (flatten) when the @normalize directive is used in a DQL query
 func (cc Config) WithLimitNormalizeNode(d int) Config {
 	cc.limitNormalizeNode = d
 	return cc
