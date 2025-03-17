0f899decc6282845b1d8ef769805bd64d09251e8
Matthew McNeely <matthew.mcneely@gmail.com>
2025-03-17 16:15:44 -0400

    Alias identical structs; add helpers
    Add tests

--- api/types.go
+++ api/types.go
@@ -10,7 +10,20 @@ type Polygon struct {
 	Coordinates [][][]float64 `json:"coordinates,omitempty"`
 }
 
-type MultiPolygon struct {
-	Type        string        `json:"type,omitempty"`
-	Coordinates [][][]float64 `json:"coordinates,omitempty"`
+type MultiPolygon = Polygon
+
+func NewPolygon(coordinates [][]float64) *Polygon {
+	polygon := &Polygon{
+		Type:        "Polygon",
+		Coordinates: [][][]float64{coordinates},
+	}
+	return polygon
+}
+
+func NewMultiPolygon(coordinates [][][]float64) *MultiPolygon {
+	multiPolygon := &MultiPolygon{
+		Type:        "MultiPolygon",
+		Coordinates: coordinates,
+	}
+	return multiPolygon
 }
